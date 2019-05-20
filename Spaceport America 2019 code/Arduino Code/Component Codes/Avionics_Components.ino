5ce2173633d6a36eda159e0c31faf9a47e7c127b
trevortai <44620588+trevortai@users.noreply.github.com>
2019-05-20 14:22:02 -0400

    Update Avionics_Components.ino

--- Spaceport America 2019 code/Arduino Code/Component Codes/Avionics_Components.ino
+++ Spaceport America 2019 code/Arduino Code/Component Codes/Avionics_Components.ino
@@ -1,5 +1,5 @@
 // Current Implemented Components:
-// IMU, Pressure & Temp Sensor, SD Card Reader
+// IMU, Pressure & Temp Sensor, SD Card Reader, RFD, GPS, Buzzer
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_GPS.h>
@@ -11,47 +11,37 @@ Adafruit_BMP280 bmp; // I2C
 #define SerialPort Serial
 MPU9250_DMP imu;
 
-SoftwareSerial mySerial(11, 10); // For gps serial
+SoftwareSerial mySerial(10, 11); //  GPS TX, RX --> Controller RX, TX
 Adafruit_GPS GPS(&mySerial);
 
 #include <SD.h>
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+int CSpin = 53;
 File dataFile;
-
-// CS 50
-// DI 51
-// DO 52
-// CLK 53
 
 void setup() {
   Serial.begin(115200);
-  GPS.begin(115200);
 
   /* ---------------------------- SD Log ----------------------------- */
   // Pins for Mega2560
-  // CS 50
+  // CS 53
   // DI 51
-  // DO 52
-  // CLK 53
+  // DO 50
+  // CLK 52
   // black = scl, white = sca
-//  pinMode(50,OUTPUT);
-//  digitalWrite(50, HIGH);
-  Serial.print("Initializing SD card...");
-  delay(1000);
+
   // make sure that the default chip select pin is set to
   // output, even if you don't use it:
-  pinMode(SS, OUTPUT);
-
+  pinMode(CSpin, OUTPUT);
+  delay(2000);
   // see if the card is present and can be initialized:
-  if (!SD.begin(50, 51, 52, 53)) {
+  if (!SD.begin(CSpin)) {
     Serial.println("Card failed, or not present");
-    // don't do anything more:
-    while (1) {
-      if (SD.begin(50, 51, 52, 53)) {
-        break;
-      }
-    }
+    while (1);
   }
-  Serial.println("card initialized.");
+
 
   // Open up the file we're going to log to!
   dataFile = SD.open("datalog.txt", FILE_WRITE);
@@ -63,7 +53,6 @@ void setup() {
 
   /* ----------------------------- Sensor --------------------------- */
 
-  Serial.println(F("Avionics Sensor and IMU"));
 
   if (!bmp.begin()) {
     Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
@@ -94,7 +83,7 @@ void setup() {
   // Gyro options are +/- 250, 500, 1000, or 2000 dps
   imu.setGyroFSR(2000); // Set gyro to 2000 dps
   // Accel options are +/- 2, 4, 8, or 16 g
-  imu.setAccelFSR(2); // Set accel to +/-2g
+  imu.setAccelFSR(16); // Set accel to +/-16g
   // Note: the MPU-9250's magnetometer FSR is set at
   // +/- 4912 uT (micro-tesla's)
 
@@ -112,70 +101,73 @@ void setup() {
   // set using the setCompassSampleRate() function.
   // This value can range between: 1-100Hz
   imu.setCompassSampleRate(10); // Set mag rate to 10Hz
+  
   /* ----------------------------- GPS ------------------------------- */
-  GPS.begin(9600);
-  // Setup recommended minimum data collection
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  // Set the update rate
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+    GPS.begin(9600);
+    // Setup recommended minimum data collection
+    GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+    // Set the update rate
+    GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
 
 }
 
+uint32_t timer = millis(); // uses millis function for timing
+float interval = 10000; // sets interval time in seconds
+
 void loop() {
-  printSensorData(); // Sensor Data output
-
-  if ( imu.dataReady() )
-  {
-    imu.update(UPDATE_ACCEL | UPDATE_GYRO | UPDATE_COMPASS);
-    printIMUData(); // IMU Data output
-  }
-
+ 
   char c = GPS.read();
-
   if (GPS.newNMEAreceived()) {
     if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
       return;  // we can fail to parse a sentence in which case we should just wait for another
   }
+  
+  // if millis() resets, just reset timer
+  if (timer > millis())
+    timer = millis();
+  // approximately every interval or so, print out the current data set
+  if (millis() - timer > interval) {
+    timer = millis(); // reset the timer
 
-  if (GPS.fix) {
+    if (GPS.fix) { // GPS Data output
+      Serial.println(GPS.latitudeDegrees, 4);
+      Serial.println(GPS.longitudeDegrees, 4);
+      Serial.println(GPS.altitude);
+      Serial.println(GPS.speed); // knots
+      Serial.println((int)GPS.satellites);
+    }
+    else { // return 0s if gps doesn't have a fix
+      Serial.println(0);
+      Serial.println(0);
+      Serial.println(0);
+      Serial.println(0);
+      Serial.println(0);
+    }
+        
+    printSensorData(); // Sensor Data output
 
-    Serial.print(GPS.latitudeDegrees, 4);
-    Serial.print(", ");
-    Serial.println(GPS.longitudeDegrees, 4);
-    Serial.print("Altitude: "); Serial.println(GPS.altitude);
-    Serial.println(" ");
-
-  }
-  else {
-    Serial.print("No GPS Fix");
-    Serial.println(" ");
-    Serial.println(" ");
+    if (imu.dataReady())
+    {
+      imu.update(UPDATE_ACCEL | UPDATE_GYRO | UPDATE_COMPASS);
+      printIMUData(); // IMU Data output
+    }
+    tone(8, 1047, 200); // plays a C6 for 200 ms
   }
 
   dataFile.flush();
-  delay(5000);
 }
 
 void printSensorData(void) {
   // Write to Serial
-  Serial.print(F("Temperature = "));
-  Serial.print(bmp.readTemperature());
-  Serial.println(" *C");
-
-  Serial.print(F("Pressure = "));
-  Serial.print(bmp.readPressure());
-  Serial.println(" Pa");
-
-  Serial.print(F("Approx altitude = "));
-  Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
-  Serial.println(" m");
-
-  Serial.println();
-
+  static float relP = bmp.readPressure() / 100; // sets relative pressure point from initial pressure reading
+  Serial.println(bmp.readTemperature()); // C
+  Serial.println(bmp.readPressure()); // Pa
+  // Toronto = 997 hPa, Albaquerque = 1009 hPa
+  Serial.println(bmp.readAltitude(relP)); // Relative altitude, m
   // Write to file
   dataFile.println(bmp.readTemperature());
   dataFile.println(bmp.readPressure());
-  dataFile.println(bmp.readAltitude(1013.25));
+  dataFile.println(bmp.readAltitude(relP));
 }
 
 void printIMUData(void)
@@ -187,34 +179,28 @@ void printIMUData(void)
   // Use the calcAccel, calcGyro, and calcMag functions to
   // convert the raw sensor readings (signed 16-bit values)
   // to their respective units.
-  float accelX = imu.calcAccel(imu.ax);
-  float accelY = imu.calcAccel(imu.ay);
-  float accelZ = imu.calcAccel(imu.az);
-  float gyroX = imu.calcGyro(imu.gx);
-  float gyroY = imu.calcGyro(imu.gy);
-  float gyroZ = imu.calcGyro(imu.gz);
-  float magX = imu.calcMag(imu.mx);
-  float magY = imu.calcMag(imu.my);
-  float magZ = imu.calcMag(imu.mz);
 
-  SerialPort.println("Accel: " + String(accelX) + ", " +
-                     String(accelY) + ", " + String(accelZ) + " g");
-  SerialPort.println("Gyro: " + String(gyroX) + ", " +
-                     String(gyroY) + ", " + String(gyroZ) + " dps");
-  SerialPort.println("Mag: " + String(magX) + ", " +
-                     String(magY) + ", " + String(magZ) + " uT");
-  SerialPort.println("Time: " + String(imu.time) + " ms");
-  SerialPort.println();
+  // Write to Serial
+  SerialPort.println(imu.calcAccel(imu.ax)); // g
+  SerialPort.println(imu.calcAccel(imu.ay));
+  SerialPort.println(imu.calcAccel(imu.az));
+  SerialPort.println(imu.calcGyro(imu.gx)); // dps
+  SerialPort.println(imu.calcGyro(imu.gy));
+  SerialPort.println(imu.calcGyro(imu.gz));
+  SerialPort.println(imu.calcMag(imu.mx)); // uT
+  SerialPort.println(imu.calcMag(imu.my));
+  SerialPort.println(imu.calcMag(imu.mz));
+  SerialPort.println(imu.time); // ms
 
   // Write to file
-  dataFile.println(accelX);
-  dataFile.println(accelY);
-  dataFile.println(accelZ);
-  dataFile.println(gyroX);
-  dataFile.println(gyroY);
-  dataFile.println(gyroZ);
-  dataFile.println(magX);
-  dataFile.println(magY);
-  dataFile.println(magZ);
-  dataFile.println(imu.time);
+  dataFile.println(imu.calcAccel(imu.ax)); // g
+  dataFile.println(imu.calcAccel(imu.ay));
+  dataFile.println(imu.calcAccel(imu.az));
+  dataFile.println(imu.calcGyro(imu.gx)); // dps
+  dataFile.println(imu.calcGyro(imu.gy));
+  dataFile.println(imu.calcGyro(imu.gz));
+  dataFile.println(imu.calcMag(imu.mx)); // uT
+  dataFile.println(imu.calcMag(imu.my));
+  dataFile.println(imu.calcMag(imu.mz));
+  dataFile.println(imu.time); // ms
 }
