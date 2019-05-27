bed580e171176cd17e9a837c0ac718ab09fc11b9
trevortai <44620588+trevortai@users.noreply.github.com>
2019-05-27 18:29:56 -0400

    Add files via upload

--- Spaceport America 2019 code/GroundStationTest/GroundStationTest.py
+++ Spaceport America 2019 code/GroundStationTest/GroundStationTest.py
@@ -1,78 +1,167 @@
+# source: https://www.thepoorengineer.com/en/python-gui/#gui
+
+from threading import Thread
 import serial
-import numpy
-import matplotlib as plt
-from drawnow import * # function for live graphs
+import time
+import collections
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import struct
+import copy
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+import tkinter as Tk
+from tkinter.ttk import Frame
+from tkinter import ttk
+import pandas as pd
 
-# Definitions
-com = 'COM5'
-baud = 115200 # baudrate
-to = 60 # timeout, s
-nData = 17 # number of data points
-ser = serial.Serial(com, baudrate = baud, timeout = to)
-data = [0]*nData # initialize array of zeros for number of data points
-plt.ion() # allows live data plots
-maxP = 50; # max number of data points shown on graph
-i = 0
-j = 0
+# File initialization
+#file_dir = os.path.dirname(os.path.abspath(__file__))
+#csv_folder = 'data files'
+#file_path = os.path.join(file_dir, csv_folder, 'csvData.csv')
+#HEADER = ['Time (ms)', 'Latitude','Longitude','Speed (kt)',
+#          'AccelX (g)','AccelY','AccelZ',
+#          'GyroX (dps)','GyroY','GyroZ',
+#          'MagX (uT)','MagY','MagZ',
+#          'Temp (C)','Pressure (Pa)','Alt (m)']
 
-# Initilize Data Arrays
-Lat1 = []; Lng1 = []; Spd1 = []; Sat1 = [];
-Tmp1 = []; Prs1 = []; Alt1 = []; 
-Acx1 = []; Acy1 = []; Acz1 = []; 
-Gyx1 = []; Gyy1 = []; Gyz1 = [];
-Mgx1 = []; Mgy1 = []; Mgz1 = []; t1 = [];
+class serialPlot:
+    def __init__(self, serialPort, serialBaud, plotLength, dataNumBytes, numData, IMU_numPlots):
+        self.port = serialPort
+        self.baud = serialBaud
+        self.plotMaxLength = plotLength
+        self.dataNumBytes = dataNumBytes
+        self.numData = numData
+        self.IMU_numPlots = IMU_numPlots
+        self.rawData = bytearray(numData * dataNumBytes)
+        self.dataType = None
+        if dataNumBytes == 2:
+            self.dataType = 'h'     # 2 byte integer
+        elif dataNumBytes == 4:
+            self.dataType = 'f'     # 4 byte float
+        self.data = []
+        for i in range(numData):   # give an array for each type of data and store them in a list
+            self.data.append(collections.deque([0] * plotLength, maxlen=plotLength))
+        self.isRun = True
+        self.isReceiving = False
+        self.thread = None
+        self.plotTimer = 0
+        self.previousTimer = 0
+        # self.csvData = []
 
-def makeFig(): # draws figures
-    #plt.figure(1) # temp data
-    #plt.ylim(20,30)
-    #plt.title('Temp Data')
-    #plt.grid(True)
-    #plt.ylabel('Temp (C)')
-    #plt.plot(Tmp1, 'r-')
+        print('Trying to connect to: ' + str(serialPort) + ' at ' + str(serialBaud) + ' BAUD.')
+        try:
+            self.serialConnection = serial.Serial(serialPort, serialBaud, timeout=4)
+            print('Connected to ' + str(serialPort) + ' at ' + str(serialBaud) + ' BAUD.')
+        except:
+            print("Failed to connect with " + str(serialPort) + ' at ' + str(serialBaud) + ' BAUD.')
 
-    plt.figure(1) # accel data
-    plt.ylim(-1.5,1.5)
-    plt.title('Accel Data')
-    plt.grid(True)
-    plt.ylabel('Accel (g)')
-    plt.plot(Acx1, 'b-', label='Accel_x')
-    plt.plot(Acy1, 'r-', label='Accel_y')
-    plt.plot(Acz1, 'g-', label='Accel_z')
-    plt.legend(loc='upper right')
+    def readSerialStart(self):
+        if self.thread == None:
+            self.thread = Thread(target=self.backgroundThread)
+            self.thread.start()
+            # Block till we start receiving values
+            while self.isReceiving != True:
+                time.sleep(0.1)
 
-while 1: # main loop
-    # [Lat, Long, Speed (knots), numSatellites, 
-    # Temp (C), Pressure (Pa), Alt (m), AccelX (g), AccelY, AccelZ
-    # GyroX (dps), GyroY, GyroZ, MagX (uT), MagY, MagZ, Time (ms)]
+    def getIMUData(self, frame, lines, lineValueText, lineLabel, timeText, byteIndex):
+        currentTimer = time.process_time()
+        self.plotTimer = int((currentTimer - self.previousTimer) * 1000)     # the first reading will be erroneous
+        self.previousTimer = currentTimer
+        timeText.set_text('Plot Interval = ' + str(self.plotTimer) + 'ms')
+        privateData = copy.deepcopy(self.rawData[:])    # so that the 3 values in our plots will be synchronized to the same sample time
+        for i in range(self.IMU_numPlots):
+            data = privateData[(i*self.dataNumBytes + byteIndex):(self.dataNumBytes + i*self.dataNumBytes + byteIndex)]
+            value,  = struct.unpack(self.dataType, data)
+            self.data[i].append(value)    # we get the latest data point and append it to our array
+            lines[i].set_data(range(self.plotMaxLength), self.data[i])
+            lineValueText[i].set_text('[' + lineLabel[i] + '] = ' + str(value))
+        #self.csvData.append([self.data[0][-1],self.data[1][-1],self.data[2][-1],
+        #                     self.data[3][-1],self.data[4][-1],self.data[5][-1],
+        #                     self.data[6][-1],self.data[7][-1],self.data[8][-1],
+        #                     self.data[9][-1],self.data[10][-1],self.data[11][-1],
+        #                     self.data[12][-1],self.data[13][-1],self.data[14][-1],
+        #                     self.data[15][-1]])
+        #df = pd.DataFrame(self.csvData)
+        #df.to_csv(file_path, header=HEADER, index=False)
+    def backgroundThread(self):    # retrieve data
+        time.sleep(1.0)  # give some buffer time for retrieving data
+        self.serialConnection.reset_input_buffer()
+        while (self.isRun):
+            self.serialConnection.readinto(self.rawData)
+            self.isReceiving = True
+            #print(self.rawData)
+
+    def close(self):
+        self.isRun = False
+        self.thread.join()
+        self.serialConnection.close()
+        print('Disconnected...')
+
+
+
+class Window(Frame):
+    def __init__(self, figure, master, SerialReference):
+        Frame.__init__(self, master)
+        self.entry = None
+        self.setPoint = None
+        self.master = master        # a reference to the master window
+        self.serialReference = SerialReference      # keep a reference to our serial connection so that we can use it for bi-directional communicate from this class
+        self.initWindow(figure)     # initialize the window with our settings
+
+    def initWindow(self, figure):
+        self.master.title("Real Time Plot")
+        canvas = FigureCanvasTkAgg(figure, master=self.master)
+        toolbar = NavigationToolbar2Tk(canvas, self.master)
+        canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+
+
+
+def main():
+    portName = 'COM4'
+    # portName = '/dev/ttyUSB0'
+    baudRate = 115200
+    maxPlotLength = 100     # number of points in x-axis of real time plot
+    dataNumBytes = 4        # number of bytes of 1 data point
+    numData = 16            # number of data points
+    IMU_numPlots = 3            # number of plots in 1 graph
+    s = serialPlot(portName, baudRate, maxPlotLength, dataNumBytes, numData, IMU_numPlots)   # initializes all required variables
+    s.readSerialStart()                                               # starts background thread
+
+    # plotting starts below
+    pltInterval = 50    # Period at which the plot animation updates [ms]
+    xmin = 0
+    xmax = maxPlotLength
+    ymin = -(1)
+    ymax = 1
+    fig = plt.figure(figsize=(10, 8))
+    ax = plt.axes(xlim=(xmin, xmax), ylim=(float(ymin - (ymax - ymin) / 10), float(ymax + (ymax - ymin) / 10)))
+    ax.set_title('Arduino Accelerometer')
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Accelerometer Output")
+
+    # put our plot onto Tkinter's GUI
+    root = Tk.Tk()
+    app = Window(fig, root, s)
+
+    lineLabel = ['X', 'Y', 'Z']
+    style = ['r-', 'c-', 'b-']  # linestyles for the different plots
+    timeText = ax.text(0.70, 0.95, '', transform=ax.transAxes)
+    lines = []
+    lineValueText = []
+    byteIndex = 16
+    for i in range(IMU_numPlots):
+        lines.append(ax.plot([], [], style[i], label=lineLabel[i])[0])
+        lineValueText.append(ax.text(0.70, 0.90-i*0.05, '', transform=ax.transAxes))
+    anim = animation.FuncAnimation(fig, s.getIMUData, fargs=(lines, lineValueText, lineLabel, timeText, byteIndex), interval=pltInterval)    # fargs has to be a tuple
     
-    while (ser.inWaiting() == 0): # waits for available data
-        pass # do nothing
-    for j in range(0,nData):
-        Rdata = ser.readline().decode().strip('\r\n') # decodes data and removes \r\n delimiters
-        Rdata = float(Rdata) # converts string readings to float
-        data[j] = Rdata
-        print(data)
+    plt.legend(loc="upper left")
 
-    # Data separation into arrays
-    Lat = data[0]; Lng = data[1]; Spd = data[2]; Sat = data[3];
-    Tmp = data[4]; Prs = data[5]; Alt = data[6]; 
-    Acx = data[7]; Acy = data[8]; Acz = data[9]; 
-    Gyx = data[10]; Gyy = data[11]; Gyz = data[12];
-    Mgx = data[13]; Mgy = data[14]; Mgz = data[15]; t = data[16];
 
-    # Appends arrays per loop
-    Lat1.append(Lat); Lng1.append(Lng); Spd1.append(Spd); Sat1.append(Sat);
-    Tmp1.append(Tmp); Prs1.append(Prs); Alt1.append(Alt); 
-    Acx1.append(Acx); Acy1.append(Acy); Acz1.append(Acz); 
-    Gyx1.append(Gyx); Gyy1.append(Gyy); Gyz1.append(Gyz);
-    Mgx1.append(Mgx); Mgy1.append(Mgy); Mgz1.append(Mgz); t1.append(t);
 
-    drawnow(makeFig)
-    plt.pause(0.00001) # required for drawnow
-    i = i+1
-    if (i > maxP): # deletes the first data points, shifting newer data over, results in moving graph
-        Lat1.pop(0); Lng1.pop(0); Spd1.pop(0); Sat1.pop(0);
-        Tmp1.pop(0); Prs1.pop(0); Alt1.pop(0); 
-        Acx1.pop(0); Acy1.pop(0); Acz1.pop(0); 
-        Gyx1.pop(0); Gyy1.pop(0); Gyz1.pop(0);
-        Mgx1.pop(0); Mgy1.pop(0); Mgz1.pop(0); t1.pop(0);     
+    root.mainloop()   # use this instead of plt.show() since we are encapsulating everything in Tkinter
+
+    s.close()
+
+
+if __name__ == '__main__':
+    main()
